09a496ea968307d384526ba034afddd4396bb2a3
Shayan <info@shayanacademy.com>
2025-01-06 17:47:42 +0100

    Customise the theme, apply custom color scheme

--- app/src/main/java/com/shayan/android101/ui/screens/ProductScreen.kt
+++ app/src/main/java/com/shayan/android101/ui/screens/ProductScreen.kt
@@ -1,31 +1,36 @@
 package com.shayan.android101.ui.screens
 
+import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.shayan.android101.ui.theme.Android101Theme
 
 @Composable
 fun ProductScreen() {
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.background)
+            .fillMaxSize()
     ) {
         Column {
             Text(
                 text = "This Jacket That Makes You Cooler Than the Weather",
-                color = Color(0xFFff9239),
+                color = MaterialTheme.colorScheme.primary,
                 style = TextStyle(
                     fontSize = 22.sp,
                     fontWeight = FontWeight.Bold
@@ -37,7 +42,7 @@ fun ProductScreen() {
 
             Text(
                 text = "Step into ultimate style and comfort with this versatile jacket. Whether you're braving chilly winds or just pretending it’s cold enough to look this good, this jacket's got you covered—literally. Bonus: It has pockets. Yes, REAL ones. \uD83D\uDD25",
-                color = Color.White,
+                color = MaterialTheme.colorScheme.onBackground,
                 style = TextStyle(
                     fontSize = 18.sp,
                 ),
@@ -46,8 +51,8 @@ fun ProductScreen() {
         }
 
         Text(
-            text = "$ 9.9",
-            color = Color(0xFFdfff00),
+            text = "$ 9.90",
+            color = MaterialTheme.colorScheme.secondary,
             style = TextStyle(
                 fontSize = 28.sp,
                 fontWeight = FontWeight.Bold
@@ -59,7 +64,7 @@ fun ProductScreen() {
 
         Text(
             text = "4.5 (340 ratings)",
-            color = Color.White,
+            color = MaterialTheme.colorScheme.onBackground,
             style = TextStyle(
                 fontSize = 18.sp,
             ),
@@ -71,8 +76,18 @@ fun ProductScreen() {
 
 }
 
-@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-fun ProductScreenPreview() {
-    ProductScreen()
+fun ProductScreenDarkPreview() {
+    Android101Theme {
+        ProductScreen()
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+fun ProductScreenLightPreview() {
+    Android101Theme {
+        ProductScreen()
+    }
 }
--- app/src/main/java/com/shayan/android101/ui/theme/Theme.kt
+++ app/src/main/java/com/shayan/android101/ui/theme/Theme.kt
@@ -1,27 +1,28 @@
 package com.shayan.android101.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Orange500,
+    secondary = Green1,
+    tertiary = Yellow,
+    background = BlueDark,
+    onBackground = Color.White,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Orange700,
+    secondary = Green2,
+    tertiary = Yellow,
+    background = BlueLight,
+    onBackground = Color.Black,
 
-    /* Other default colors to override
+/* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
@@ -29,22 +30,15 @@ private val LightColorScheme = lightColorScheme(
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
+*/
 )
 
 @Composable
 fun Android101Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
