4992e02ae2f630757a04125aae3fd16fb740a613
Shayan <info@shayanacademy.com>
2025-01-11 13:01:32 +0100

    add AsyncImage

--- app/src/main/java/com/shayan/android101/ui/screens/ProductScreen.kt
+++ app/src/main/java/com/shayan/android101/ui/screens/ProductScreen.kt
@@ -6,62 +6,81 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import coil.compose.AsyncImage
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.shayan.android101.ui.theme.Android101Theme
 import com.shayan.android101.ui.theme.SpacingM
 import com.shayan.android101.ui.theme.SpacingXL
 
 @Composable
 fun ProductScreen() {
-    Box(
-        modifier = Modifier
-            .background(MaterialTheme.colorScheme.background)
-            .fillMaxSize()
-    ) {
-        Column {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.background)
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            Column {
+                AsyncImage(
+                    model = "https://fakestoreapi.shayanaryan.com/img/51eg55uWmdL.jpg",
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(300.dp)
+                )
+
+                Spacer(modifier = Modifier.height(SpacingM))
+
+                Text(
+                    text = "T-Shirt of Infinite Comfort and Questionable Decisions",
+                    color = MaterialTheme.colorScheme.primary,
+                    style = MaterialTheme.typography.headlineSmall,
+                    modifier = Modifier.padding(SpacingM)
+                )
+
+                Spacer(modifier = Modifier.height(SpacingM))
+
+                Text(
+                    text = "This t-shirt doesn't just sit on your torso—it defines your vibe. Perfect for lazy Sundays, or making that one friend ask, \"What's going on?\".\nSoft, breathable, and questionable in all the right ways. Warning: May attract compliments. \uD83E\uDD9D\uD83D\uDD25",
+                    color = MaterialTheme.colorScheme.onBackground,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(horizontal = SpacingM)
+                )
+            }
+
             Text(
-                text = "This Jacket That Makes You Cooler Than the Weather",
-                color = MaterialTheme.colorScheme.primary,
-                style = MaterialTheme.typography.headlineSmall,
-                modifier = Modifier.padding(SpacingM)
+                text = "$ 19.99",
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(start = SpacingM, bottom = SpacingXL)
             )
 
-            Spacer(modifier = Modifier.height(SpacingM))
-
             Text(
-                text = "Step into ultimate style and comfort with this versatile jacket. Whether you're braving chilly winds or just pretending it’s cold enough to look this good, this jacket's got you covered—literally. Bonus: It has pockets. Yes, REAL ones. \uD83D\uDD25",
+                text = "4.5 (238 ratings)",
                 color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.padding(horizontal = SpacingM)
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(end = SpacingM, bottom = SpacingXL)
             )
         }
-
-        Text(
-            text = "$ 9.90",
-            color = MaterialTheme.colorScheme.secondary,
-            style = MaterialTheme.typography.headlineMedium,
-            modifier = Modifier
-                .align(Alignment.BottomStart)
-                .padding(start = SpacingM, bottom = SpacingXL)
-        )
-
-        Text(
-            text = "4.5 (340 ratings)",
-            color = MaterialTheme.colorScheme.onBackground,
-            style = MaterialTheme.typography.bodyLarge,
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(end = SpacingM, bottom = SpacingXL)
-        )
     }
-
 }
 
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
