4f1787c9eeed884246835b69422c364ec8e067ec
Shayan <info@shayanacademy.com>
2025-01-15 17:33:45 +0100

    remove Dispatcher

--- app/src/main/java/com/shayan/android101/viewmodels/ProductViewModel.kt
+++ app/src/main/java/com/shayan/android101/viewmodels/ProductViewModel.kt
@@ -4,11 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.shayan.android101.datamodel.Product
 import com.shayan.android101.network.FakeStoreApi
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.stateIn
 
 class ProductViewModel : ViewModel() {
@@ -16,7 +14,5 @@ class ProductViewModel : ViewModel() {
 
     val product: StateFlow<Product?> = flow {
         emit(api.getProduct(id = 1))
-    }
-        .flowOn(Dispatchers.IO)
-        .stateIn(viewModelScope, SharingStarted.Lazily, null)
+    }.stateIn(viewModelScope, SharingStarted.Lazily, null)
 }
