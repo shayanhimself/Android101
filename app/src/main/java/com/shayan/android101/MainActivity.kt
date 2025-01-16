239d33211e455d2eadca21108224a582421fda47
Shayan <info@shayanacademy.com>
2025-01-16 18:06:04 +0100

    observe the product

--- app/src/main/java/com/shayan/android101/MainActivity.kt
+++ app/src/main/java/com/shayan/android101/MainActivity.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.ViewModelProvider
 import com.shayan.android101.ui.screens.ProductScreen
 import com.shayan.android101.ui.theme.Android101Theme
+import com.shayan.android101.viewmodels.ProductViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val productViewModel = ViewModelProvider(this)[ProductViewModel::class.java]
+
         enableEdgeToEdge()
         setContent {
             Android101Theme {
-                ProductScreen()
+                ProductScreen(productViewModel)
             }
         }
     }
