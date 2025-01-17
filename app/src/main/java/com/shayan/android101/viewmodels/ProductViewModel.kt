939cdb63136925fd881259faa2dbd0354781b285
Shayan <info@shayanacademy.com>
2025-01-17 09:30:09 +0100

    error handling

--- app/src/main/java/com/shayan/android101/viewmodels/ProductViewModel.kt
+++ app/src/main/java/com/shayan/android101/viewmodels/ProductViewModel.kt
@@ -6,12 +6,14 @@ import com.shayan.android101.datamodel.Product
 import com.shayan.android101.network.FakeStoreApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.catch
 
 class ProductViewModel : ViewModel() {
 
     class ViewState(
         val product: Product? = null,
         val isLoading: Boolean = false,
+        val hasError: Boolean = false,
     )
 
     private val api = FakeStoreApi()
@@ -19,8 +21,12 @@ class ProductViewModel : ViewModel() {
     val viewState = MutableStateFlow(ViewState(isLoading = true))
 
     private fun fetchProduct() = viewModelScope.launch {
-        val product = api.getProduct(id = 2)
-        viewState.value = ViewState(product = product)
+        try {
+            val product = api.getProduct(id = 2)
+            viewState.value = ViewState(product = product)
+        } catch (e: Exception) {
+            viewState.value = ViewState(hasError = true)
+        }
     }
 
     init {
