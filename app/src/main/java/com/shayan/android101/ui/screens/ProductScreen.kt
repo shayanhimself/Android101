8992b4cf62f972156ffd65b500137ac5e05b608f
Shayan <info@shayanacademy.com>
2025-01-27 18:28:58 +0100

    lint

--- app/src/main/java/com/shayan/android101/ui/screens/ProductScreen.kt
+++ app/src/main/java/com/shayan/android101/ui/screens/ProductScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -128,7 +127,7 @@ private fun ProductContent(
             Spacer(modifier = Modifier.height(SpacingXXXXL))
         }
 
-        Box (
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(SpacingXXXXL)
